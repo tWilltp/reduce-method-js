564d95c6d972e2021cc22531f423204291605d41
tWilltp <williamjenkinsfeb@gmail.com>
2023-03-06 17:19:02 +0000

    reduce method js

--- reduce.js
+++ reduce.js
@@ -7,6 +7,16 @@
 
 // Summing an array of numbers:
 
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => {
+    console.log(
+        'accumulator:', acc,
+        'current value:', curr,
+        'total:', acc + curr
+    );
+    return acc + curr;
+}, 0); // }>, 0<); << explicitly specified value should be put in (but with 0 it is not necessary, just good practice)
+console.log(sum);
 
 const teamMembers = [
   {
@@ -33,5 +43,20 @@ const teamMembers = [
 
 // Totaling a specific object property
 
+let totalExp = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0); // initial value specified >> , 0
+console.log(totalExp);
+
 
 // Grouping by a property, and totaling it too
+
+let expByProf = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}, {});
+
+console.log(expByProf);
